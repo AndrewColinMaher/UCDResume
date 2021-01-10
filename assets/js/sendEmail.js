85f2bbcadd5a282d994e1e0d1dec70a72dd1b544
AndrewColinMaher <colinmaher1710@yahoo.ie>
2021-01-10 18:18:35 +0000

    added email.js git status

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+// prettier-ignore
+function sendMail(contactForm) {
+    emailjs.send("service_9jvgzdd","template_lldf27k", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
